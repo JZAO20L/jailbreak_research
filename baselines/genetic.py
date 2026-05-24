4e399fccf3e39e57c89f16acce8257a14b0f32b2
JZAO20L <2585959782@qq.com>
2026-05-24 21:36:55 +0800

    修复GeneticAttacker参数重复传递问题

--- baselines/genetic.py
+++ baselines/genetic.py
@@ -63,6 +63,10 @@ class GeneticAttacker(BaseAttacker):
             crossover_rate: Probability of crossover
             elite_size: Number of top individuals to keep unchanged
         """
+        # 移除kwargs中可能存在的max_iterations，避免重复传递
+        if 'max_iterations' in kwargs:
+            max_generations = kwargs.pop('max_iterations')
+        
         super().__init__(max_iterations=max_generations, verbose=verbose, **kwargs)
         self.population_size = population_size
         self.max_generations = max_generations
